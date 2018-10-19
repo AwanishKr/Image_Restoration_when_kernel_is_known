b1214fd707777f4b4903239899785034742cd048
Awanish Kumar <awanishk389@gmail.com>
2018-10-19 22:28:38 +0530

    Update working_full_inverse.py

--- working_full_inverse.py
+++ working_full_inverse.py
@@ -2,10 +2,10 @@ import numpy as np
 import cv2
 from scipy import signal
 import matplotlib.pyplot as plt
-# from skimage.measure import compare_ssim as ssim
-# from skimage.measure import structural_similarity as ssim
-from skimage import measure
+from skimage.measure import compare_ssim as ssim
+from skimage.measure import structural_similarity as ssim
 
+# this defined function for ssim was not working as expected so i used in built function 
 # def ssim(im1, im2, k=(0.01, 0.03), l=255):
 #     c1 = (k[0]*l)**2
 #     c2 = (k[1]*l)**2
@@ -20,7 +20,7 @@ from skimage import measure
 #     SSIM = ((2*mu_im1*mu_im2 + c1)*(2*cov_im1_im2 + c2))/((mu_im1**2 + mu_im2**2 + c1)*(var_im1 + var_im2 +c2))
 
 #     return SSIM
-
+# here kernel size and image sizes are made equal and then basic steps of frequency domain filtering is done
 def inverse_filter(blurred_image, kernel):
 	rows, cols, channel = blurred_image.shape
 	# padding of original image
@@ -56,19 +56,23 @@ if __name__ == '__main__':
 	blurred_image = cv2.imread("Blurry1_4.jpg", 1)
 	blurred_image = cv2.normalize(blurred_image, None, 0.0, 1.0, cv2.NORM_MINMAX, cv2.CV_32F)
 
-	# ground_truth = cv2.imread("GroundTruth1_1_1.jpg", 1)
-	# ground_truth = cv2.normalize(ground_truth, None, 0.0, 1.0, cv2.NORM_MINMAX, cv2.CV_32F)
+	ground_truth = cv2.imread("GroundTruth1_1_1.jpg", 1)
+	ground_truth = cv2.normalize(ground_truth, None, 0.0, 1.0, cv2.NORM_MINMAX, cv2.CV_32F)
 	
 
 	kernel = cv2.imread("blur_kern_4.png", 1)
+	# this is total experiment based but reshaping gave better result in this case
 	kernel = cv2.resize(kernel, (21,21, ))
 	
 	recovered_image = inverse_filter(blurred_image, kernel)
 	recovered_image = cv2.normalize(recovered_image, None, 0.0, 1.0, cv2.NORM_MINMAX, cv2.CV_32F)
-	
-	# print("shape of blurred_image", blurred_image.shape)
-	# print("shape of ground_truth", ground_truth.shape)
 
+	SSIM_b = ssim(recovered_image[:,:,0], ground_truth[:,:,0])
+	SSIM_g = ssim(recovered_image[:,:,1], ground_truth[:,:,1])
+	SSIM_r = ssim(recovered_image[:,:,2], ground_truth[:,:,2])
+	SSIM = (SSIM_b+SSIM_g+SSIM_r)/3.0
+	print(SSIM)
+	# because cv imports images as BGR and matplotlib takes images as RGB so to avoid colour inversion colour space is changed 
 	recovered_image = cv2.cvtColor(recovered_image, cv2.COLOR_BGR2RGB)
 	fig = plt.figure()
 	plt.axis("off")
